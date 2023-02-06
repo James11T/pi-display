4295aebc9b65e39549de031cae8d730a1cd1709e
James11T <jamail090999@gmail.com>
2023-02-06 20:56:27 +0000

    add api back off

--- src/hooks/useHue.ts
+++ src/hooks/useHue.ts
@@ -1,65 +1,12 @@
 import React from "react";
 import LightColor from "../color";
+import CONFIG from "../config";
+import { HueLight } from "./HueTypes";
 import useDebounce from "./useDebounce";
 
 const { VITE_BRIDGE_URL, VITE_BRIDGE_USERNAME } = import.meta.env;
 
 const baseUrl = `${VITE_BRIDGE_URL}api/${VITE_BRIDGE_USERNAME}`;
-
-interface HueLight {
-  state: {
-    on: boolean;
-    bri: number;
-    hue: number;
-    sat: number;
-    effect: string;
-    xy: [number, number];
-    ct: number;
-    alert: string;
-    colormode: string;
-    mode: string;
-    reachable: boolean;
-  };
-  swupdate: {
-    state: string;
-    lastinstall: string;
-  };
-  type: string;
-  name: string;
-  modelid: string;
-  manufacturername: string;
-  productname: string;
-  capabilities: {
-    certified: boolean;
-    control: {
-      mindimlevel: number;
-      maxlumen: number;
-      colorgamuttype: string;
-      colorgamut: [[number, number], [number, number], [number, number]];
-      ct: {
-        min: number;
-        max: number;
-      };
-    };
-    streaming: {
-      renderer: boolean;
-      proxy: boolean;
-    };
-  };
-  config: {
-    archetype: string;
-    function: string;
-    direction: string;
-    startup: {
-      mode: string;
-      configured: boolean;
-    };
-  };
-  uniqueid: string;
-  swversion: string;
-  swconfigid: string;
-  productid: string;
-}
 
 interface Light {
   name: string;
@@ -79,8 +26,10 @@ const hueLightToLight = (light: HueLight, id: string) => ({
 
 const useHue = (focusedLight?: string) => {
   const [lights, setLights] = React.useState<Light[]>([]);
-  const updateIntervalId = React.useRef<number | undefined>(undefined);
+  const updateTimeoutID = React.useRef<number | undefined>(undefined);
+  const lastLocalChange = React.useRef<Date>(new Date(0));
 
+  // Fetch all light states from Hue API
   const getLights = async () => {
     const res = await fetch(`${baseUrl}/lights`);
     if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
@@ -88,6 +37,7 @@ const useHue = (focusedLight?: string) => {
     return data;
   };
 
+  // Fetch a single light state from Hue API
   const getLight = React.useCallback(async (id: string) => {
     const res = await fetch(`${baseUrl}/lights/${id}`);
     if (!res.ok) throw new Error(`${res.status}: ${res.statusText}`);
@@ -95,56 +45,86 @@ const useHue = (focusedLight?: string) => {
     return data;
   }, []);
 
-  const setLight = React.useCallback(async (id: string, color: LightColor, on = true) => {
-    setLights((old) =>
-      old.map((light) => {
-        if (light.id !== id) return light;
-        return { ...light, color, on };
-      })
-    );
+  // Set a single remote light state with Hue API
+  const dispatchSetLight = useDebounce(
+    React.useCallback(
+      async (id: string, color: LightColor, on = true) =>
+        await fetch(`${baseUrl}/lights/${id}/state`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            on,
+            ...color.forHue(),
+          }),
+        }),
+      []
+    ),
+    CONFIG.hue.updateDebounce
+  );
 
-    const res = await fetch(`${baseUrl}/lights/${id}/state`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        on,
-        ...color.forHue(),
-      }),
-    });
-    return await res.json();
-  }, []);
+  // Set the local state for a light
+  // Calls debounced API fetch
+  const setLight = React.useCallback(
+    async (id: string, color: LightColor, on = true) => {
+      lastLocalChange.current = new Date();
+      setLights((old) =>
+        old.map((light) => {
+          if (light.id !== id) return light;
+          return { ...light, color, on };
+        })
+      );
 
-  const updateLight = async (id: string) => {
-    const updatedLight = await getLight(id);
+      dispatchSetLight(id, color, on);
+    },
+    [dispatchSetLight]
+  );
 
-    setLights((old) =>
-      old.map((light) => {
-        if (light.id !== id) return light;
-        return hueLightToLight(updatedLight, id);
-      })
-    );
-  };
+  // Fetch and store the current state of a single light
+  const updateLight = React.useCallback(
+    async (id: string) => {
+      const updatedLight = await getLight(id);
 
-  const updateLights = async () => {
+      setLights((old) =>
+        old.map((light) => {
+          if (light.id !== id) return light;
+          return hueLightToLight(updatedLight, id);
+        })
+      );
+    },
+    [getLight]
+  );
+
+  // Fetch and store all lights
+  const updateLights = React.useCallback(async () => {
     const lights = await getLights();
 
     setLights(Object.keys(lights).map((lightId) => hueLightToLight(lights[lightId], lightId)));
-  };
+  }, []);
+
+  // Run to update light states
+  // Only fetches remote state if the users is not actively changing local state
+  const runCycle = React.useCallback(() => {
+    const now = new Date();
+    if (Number(now) - Number(lastLocalChange.current) < CONFIG.hue.changingGrace) {
+      // Has been changed by the user in the last Xms
+      updateTimeoutID.current = window.setTimeout(runCycle, CONFIG.hue.changingGrace); // "Back-off" until local state settles
+    } else {
+      // Has NOT been changed by the user in the last Xms
+      focusedLight && updateLight(focusedLight);
+      updateTimeoutID.current = window.setTimeout(runCycle, CONFIG.hue.refreshInterval);
+    }
+  }, [focusedLight, updateLight]);
 
   React.useEffect(() => {
-    updateLights();
-    updateIntervalId.current = window.setInterval(() => {
-      focusedLight && updateLight(focusedLight);
-    }, 5_000);
-    return () => window.clearInterval(updateIntervalId.current);
-  }, [focusedLight]);
+    window.clearTimeout(updateTimeoutID.current);
+    updateLights(); // Update all lights
+    updateTimeoutID.current = window.setTimeout(runCycle, CONFIG.hue.refreshInterval); // UseTimeout so interval can change
+    return () => window.clearTimeout(updateTimeoutID.current);
+  }, [runCycle, updateLights]);
 
-  const debouncedGetLight = useDebounce(getLight, 500);
-  const debouncedSetLight = useDebounce(setLight, 500);
-
-  return { lights, getLight: debouncedGetLight, setLight: debouncedSetLight };
+  return { lights, setLight };
 };
 
 export default useHue;
